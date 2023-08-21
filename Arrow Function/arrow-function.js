848b4a623e73e641bbfc57e389086d1b4247853c
imran-it1 <imranhossainsakil22@gmail.com>
2023-08-21 10:08:59 +0600

    Start learning ES6 feature

---
+++ Arrow Function/arrow-function.js
@@ -0,0 +1,34 @@
+// Normal Function
+// function number() {
+//   return 10;
+// }
+
+// Conver to Arrow Function
+// let number = () => {
+//   return 10;
+// };
+
+// If you have only one statement you can further simplify the Arrow function Syntax
+// let number = () => 10;
+
+// You can also use console.log same way if you have only one statement
+// Jehetu console.log sorasori console kore, return kore na, tai just function take call korlei output dekha jabe
+
+// let number = () => console.log(10);
+// number();
+
+// With Parameter
+
+// function number(n) {
+//   return n;
+// }
+// How can we convert this function to an Arrow function
+
+// let number = (n) => console.log(n);
+// number(10);
+
+// let number = (a, b) => a + b;
+// console.log(number(10, 5));
+
+// let number = (a, b) => console.log(a + b);
+// number(10, 5);
