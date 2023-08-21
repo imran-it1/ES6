1eeb2a3c2a7a66bb8d4f4105bf98a5b6cb029c1d
imran-it1 <imranhossainsakil22@gmail.com>
2023-08-21 18:25:05 +0600

    Ternary Operator concept

---
+++ Ternary Operator/ternary.js
@@ -0,0 +1,23 @@
+let age = 20;
+// if (age >= 18) {
+//   console.log("You are young");
+// } else {
+//   console.log("You are child");
+// }
+
+let result = age >= 18 ? "adult" : age <= 10 ? "child" : "young";
+// console.log(result);
+
+/*
+    Boolean value check
+*/
+
+let isLoggedin = false;
+// let access = isLoggedin ? "valid" : "invalid";
+// let access = isLoggedin === true ? true : false;
+
+/*
+    Only ekta statement thakle evabe simple o kora jay
+*/
+let access = isLoggedin;
+console.log(access);
